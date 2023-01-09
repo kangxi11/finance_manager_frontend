2d2e782eef60fec4ce6da2732d5638e982cfe004
Ryan Kang <rkang2009@gmail.com>
2023-01-08 20:54:44 -0500

    Fixed post request bug

--- src/Containers/Transactions/Transactions.js
+++ src/Containers/Transactions/Transactions.js
@@ -86,7 +86,7 @@ export default function Transactions(props) {
       };    
 
     const postButtonClicked = () => {
-        axios.post(`${process.env.process.env.REACT_APP_NODE_SERVER}/transactions`,
+        axios.post(`${process.env.REACT_APP_NODE_SERVER}/transactions`,
             {
                 'transactions': [
                     ...transactions.map(t => t.getJSON())
