416a7d9b4ad6377fd80932e79cf7b6b5dbdc3602
Ryan Kang <rkang2009@gmail.com>
2023-01-24 21:41:00 -0500

    Basic line chart in Charts panel

--- src/Containers/Charts/Charts.js
+++ src/Containers/Charts/Charts.js
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from 'react';
+import * as d3 from 'd3';
 
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -12,6 +13,7 @@ import { StyledTableCell, StyledTableRow } from '../../Objects/TableStyler';
 
 export default function Charts(props) {
     const [past12MonthsTransactions, setPast12MonthsTransactions] = useState([]);
+
 
     useEffect(() => {
         const today = dayjs();
@@ -42,6 +44,97 @@ export default function Charts(props) {
 
         setPast12MonthsTransactions(past12Months);
     }, []);
+
+    useEffect(() => {
+        createLineGraph();
+    }, [past12MonthsTransactions]);
+
+    const createLineGraph = async () => {
+        var margin = { top: 20, right: 20, bottom: 50, left: 70 },
+        width = 960 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
+    
+        let svg = d3.select("#line-graph")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", `translate(${margin.left},     ${margin.top})`);
+
+        // Add X axis and Y axis
+        var x = d3.scaleTime()
+            .range([0, width]);
+        var y = d3.scaleLinear().range([height, 0]);
+
+        x.domain(d3.extent(past12MonthsTransactions, (d) => { return d.date; }));
+        y.domain([0, d3.max(past12MonthsTransactions, (d) => { return d.total; })]);
+
+        svg.append("g")
+            .attr("transform", `translate(0, ${height})`)
+            .call(d3.axisBottom(x));   
+        svg.append("g")
+            .call(d3.axisLeft(y));
+
+            // add the Line
+    var totalLine = d3.line()
+        .x((d) => { return x(d.date); })
+        .y((d) => { return y(d.total); });
+
+    var rentLine = d3.line()
+        .x((d) => { return x(d.date); })
+        .y((d) => { return y(d.Rent); });
+
+    var electricityLine = d3.line()
+        .x((d) => { return x(d.date); })
+        .y((d) => { return y(d.Electricity); });
+
+    var groceriesLine = d3.line()
+        .x((d) => { return x(d.date); })
+        .y((d) => { return y(d.Groceries); });
+
+    var diningOutLine = d3.line()
+        .x((d) => { return x(d.date); })
+        .y((d) => { return y(d['Dining Out']); });
+    
+    svg.append("path")
+        .data([past12MonthsTransactions])
+        .attr("class", "line")
+        .attr("fill", "none")
+        .attr("stroke", "steelblue")
+        .attr("stroke-width", 1.5)
+        .attr("d", totalLine);
+
+    svg.append("path")
+        .data([past12MonthsTransactions])
+        .attr("class", "line")
+        .attr("fill", "none")
+        .attr("stroke", "#7a4ccf")
+        .attr("stroke-width", 1.5)
+        .attr("d", rentLine);
+
+    svg.append("path")
+        .data([past12MonthsTransactions])
+        .attr("class", "line")
+        .attr("fill", "none")
+        .attr("stroke", "#bcc44b")
+        .attr("stroke-width", 1.5)
+        .attr("d", electricityLine);
+
+    svg.append("path")
+        .data([past12MonthsTransactions])
+        .attr("class", "line")
+        .attr("fill", "none")
+        .attr("stroke", "green")
+        .attr("stroke-width", 1.5)
+        .attr("d", groceriesLine);
+
+    svg.append("path")
+        .data([past12MonthsTransactions])
+        .attr("class", "line")
+        .attr("fill", "none")
+        .attr("stroke", "#4ccf70")
+        .attr("stroke-width", 1.5)
+        .attr("d", diningOutLine);
+    }
 
     return (
         <div>
@@ -81,7 +174,7 @@ export default function Charts(props) {
                     </TableBody>
                 </Table>
             </TableContainer>
-
+            <svg id='line-graph'></svg>
         </div>
     )
 }
